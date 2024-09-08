b366e51b4dab0cca8721d8a535ae17781f4d2ec8
Diptanu Sarkar <diptanus961@gmail.com>
2024-09-08 13:27:36 +0530

    dip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,22 +2,27 @@
 
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {FaLinkedin} from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8 px-4">
-      <div className="container mx-auto text-center">
+    <footer className="bg-transparent  text-white py-8 px-4">
+      <div className="container  mx-auto text-center">
         <div className="flex flex-col lg:flex-row lg:ml-96 items-center">
           <input
             className="rounded-xl w-full max-w-md h-11 text-black pl-4 mb-4"
             type="email"
             placeholder="Enter Email"
           />
-          <button className="bg-blue-700   text-white h-11 w-60 lg:w-20 lg:ml-4 lg:mb-4 max-w-xs rounded-xl hover:bg-blue-900 transition duration-300">
+          <button className="bg-blue-700 shadow-blue-800 shadow-2xl text-white h-11 w-60 lg:w-20 lg:ml-4 lg:mb-4 max-w-xs rounded-xl hover:bg-blue-900 focus:ring-2 focus:ring-blue-400 transition duration-300">
             Submit
           </button>
         </div>
         <p className="pt-12">&copy; {new Date().getFullYear()} Diptanu. All rights reserved.</p>
+      </div>
+      <div className=" text-xl ">
+        <a className="" href="https://www.linkedin.com/in/diptanu-sarkar-0a209026a/"> <FaLinkedin className='text-blue-300' /></a>
       </div>
     </footer>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/* @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+ 
+    extend: { backgroundImage:{'my-img':"url('/lake-4839058_1920.jpg')"
+    },
+    
+  },
   },
   plugins: [],
 }
+
 
